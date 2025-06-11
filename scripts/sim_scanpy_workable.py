315a8a4572233c7c74b781dbe93b76a0028f1cd9
lspongebobjh <lspongebobjh@gmail.com>
2025-06-11 13:31:33 +0800

    clean sim_scanpy_workable

---
+++ scripts/sim_scanpy_workable.py
@@ -0,0 +1,56 @@
+from biochatter.llm_connect import GptConversation
+from biochatter.api_agent import APIAgent, ScanpyQueryBuilder, ScanpyFetcher, ScanpyInterpreter
+
+import os
+import scanpy
+from scanpy.datasets import krumsiek11
+
+from dotenv import load_dotenv
+load_dotenv()
+
+scanpy.settings.datasetdir = os.environ.get("DATA")
+scanpy.settings.figdir = os.environ.get("FIG")
+if not os.path.exists(scanpy.settings.figdir):
+    os.makedirs(scanpy.settings.figdir)
+
+system_prompt = """
+You are a professional bioinformatician. 
+1. You have access to the data object named `data`.
+2. Please only use the provided tools. Do not use any tools that are not provided.
+"""
+# Create an API agent
+query_builder_conv = GptConversation(
+    model_name=os.environ.get("MODEL"), 
+    prompts={
+        "primary_model_prompts": system_prompt
+    }
+)
+interpreter_conv = GptConversation(
+    model_name=os.environ.get("MODEL"), 
+    prompts={
+        "primary_model_prompts": system_prompt
+    }
+)
+
+query_builder_conv.set_api_key(
+    os.environ.get("OPENAI_API_KEY")
+)
+interpreter_conv.set_api_key(
+    os.environ.get("OPENAI_API_KEY")
+)
+
+scanpy_agent = APIAgent(
+    query_builder=ScanpyQueryBuilder(
+        conversation=query_builder_conv,
+    ),
+    fetcher=ScanpyFetcher(),
+    interpreter=ScanpyInterpreter(
+        conversation=interpreter_conv,
+    )
+)
+
+# Execute a query
+question = "visualize the t-SNE plot of the cells"
+data = krumsiek11()
+scanpy_agent.set_data(data)
+code_lines = scanpy_agent.execute(question)
